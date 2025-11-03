89b6c1b4144bb32d198500413ba6c409f4d0a0ef
Lakshan <lakshan.imantha02@gmail.com>
2025-11-03 08:46:51 +0530

    Implement the project structure in microservice architecture

---
+++ permission-service/src/test/java/com/lakshan/permission_service/PermissionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lakshan.permission_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermissionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
