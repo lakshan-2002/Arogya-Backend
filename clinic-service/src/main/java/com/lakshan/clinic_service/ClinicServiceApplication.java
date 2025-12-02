3b1234ce7a308458cd28754a3e82ae0ba34d71fd
Lakshan <lakshan.imantha02@gmail.com>
2025-12-02 22:22:32 +0530

    Implement registering services in eureka server

--- clinic-service/src/main/java/com/lakshan/clinic_service/ClinicServiceApplication.java
+++ clinic-service/src/main/java/com/lakshan/clinic_service/ClinicServiceApplication.java
@@ -2,10 +2,13 @@ package com.lakshan.clinic_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class ClinicServiceApplication {
 
 	public static void main(String[] args) {
