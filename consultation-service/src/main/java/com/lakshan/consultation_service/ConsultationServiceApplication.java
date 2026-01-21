9e6cdf43b23d824cc5032207c5ca222377887fcf
Lakshan <lakshan.imantha02@gmail.com>
2026-01-21 13:04:26 +0530

    Register Consultation Service in eureka server

--- consultation-service/src/main/java/com/lakshan/consultation_service/ConsultationServiceApplication.java
+++ consultation-service/src/main/java/com/lakshan/consultation_service/ConsultationServiceApplication.java
@@ -2,9 +2,11 @@ package com.lakshan.consultation_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients(basePackages = {
         "com.lakshan.consultation_service.client"
 })
